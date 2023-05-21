de0ee57d87824bcc9ab1ff94cf843aeeb1a0877f
Md Abrar Haider <133400726+abrarcuet96@users.noreply.github.com>
2023-05-21 19:31:52 +0600

    Delete Chapter_13_( More Function ) directory

--- Chapter_13_( More Function )/zip_func.py
+++
@@ -1,7 +0,0 @@
-user_id=['user1','user2','user3']
-first_names=['abrar','araf','ilham']
-last_names=['haider','haider','labib']
-print(list(zip(user_id,first_names,last_names)))
-# output-->[('user1', 'abrar', 'haider'), ('user2', 'araf', 'haider'), ('user3', 'ilham', 'labib')]
-# it can be converted to dictionary but have to use only two list.
-print(dict(zip(user_id,first_names)))
