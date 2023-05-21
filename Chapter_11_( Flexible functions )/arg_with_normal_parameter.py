3c66250417fc254048eb960373480633dbb14b3f
Md Abrar Haider <133400726+abrarcuet96@users.noreply.github.com>
2023-05-21 19:29:35 +0600

    Delete Chapter_11_( Flexible functions ) directory

--- Chapter_11_( Flexible functions )/arg_with_normal_parameter.py
+++
@@ -1,8 +0,0 @@
-def multiply_nums(num1,num2,*args):
-    multiply=1
-    for i in args:
-        multiply*=i
-    return multiply
-print(multiply_nums(2,3,3,4)) 
-# Here [2,3 are normal parameters]
-# output--> 3x4=12
--- Chapter_11_( Flexible functions )/args_as_argument.py
+++
@@ -1,8 +0,0 @@
-def multiply_nums(*args):
-    multiply=1
-    for i in args:
-        multiply*=i
-    return multiply
-
-nums=[2,3,4]
-print(multiply_nums(*nums)) # to unpack list star is used.
